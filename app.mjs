c63165f3fc292a4b468299f1b2b2f47980c12a96
illusion <17003685+fredkydd@users.noreply.github.com>
2023-04-05 23:37:12 +0400

    added switch case

--- app.mjs
+++ app.mjs
@@ -6,15 +6,47 @@ import { dirname } from "path";
 
 dotenv.config();
 const app = express(), __filename = fileURLToPath(import.meta.url), __dirname = dirname(__filename);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/toDo.html"));
 app.get("/today", (req, res) => {
-  const day = new Date();
-  (day.getDay() === 0 || day.getDate() === 21) ? res.send("Just chilling!") : res.send("You have to work");
+  const today = new Date(), currentDay = today.getDay();
+  let day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday"
+      break;
+    case 1:
+      day = "Monday"
+      break;
+    case 2:
+      day = "Tuesday"
+      break;
+    case 3:
+      day = "Wednesday"
+      break;
+    case 4:
+      day = "Thursday"
+      break;
+    case 5:
+      day = "Friday"
+      break;
+    case 6:
+      day = "Saturday"
+      break;
+
+    default:
+      console.log(`Error: current day is equal to ${currentDay}`);
+      break;
+  }
+
+  console.log(`today's ${today}\ncurrentDay's ${currentDay}\ntoday's ${today}`);
+  res.render("list", { kindaDay: day });
+  //* res.render("ejsTemplateFileName", { key: value });
 });
 
 app.listen(process.env.PORT, console.log(`server is running on ${process.env.PORT} port 🚨`));
